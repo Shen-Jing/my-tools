161f8cdee94730de5cc0170abb077b60e82c2558
Hsun-Kai <hsunkai@synopsys.com>
2022-09-18 02:49:37 -0700

    [FEAT] MS Teams Webhook for notify

--- order.py
+++ order.py
@@ -7,6 +7,30 @@ from datetime import datetime
 import pytz
 import pymsteams
 
+def send_teams(webhook_url: str, content: str, subtitle: str, title: str, color: str="000000") -> int:
+    """
+      - Send a teams notification to the desired webhook_url
+      - Returns the status code of the HTTP request
+        - webhook_url : the url you got from the teams webhook configuration
+        - content : your formatted notification content
+        - title : the message that'll be displayed as title, and on phone notifications
+        - color (optional) : hexadecimal code of the notification's top line color, default corresponds to black
+    """
+    response = requests.post(
+        url = webhook_url,
+        headers = {"Content-Type": "application/json"},
+        json = {
+            "themeColor": color,
+            "summary": title,
+            "sections": [{
+                "activityTitle": title,
+                "activitySubtitle": subtitle,
+                "text": content
+            }],
+        },
+    )
+    return response.status_code # Should be 200
+
 if datetime.today().weekday() >= 5:
     print("Today is weekend. No action.")
     sys.exit()
@@ -14,6 +38,7 @@ if datetime.today().weekday() >= 5:
 taipei_timezone = pytz.timezone("Asia/Taipei")
 taipei_date = datetime.now(taipei_timezone).strftime('%Y/%m/%d')
 url = "http://oas/leave/pages/OrderList01.jsp?queryStartDate={}&queryEndDate={}".format(taipei_date, taipei_date)
+# url = "http://oas/leave/pages/OrderList01.jsp?queryStartDate={}&queryEndDate={}".format("2022/09/16", "2022/09/16")  # For testing
 
 conf = yaml.load(open('order_auth.yaml'))  # deprecated, need to modify
 userid = conf['user']['userid']
@@ -39,5 +64,11 @@ for row in rows:
         num_users += 1
         td_list.append(uid)
 
-print(num_users)
-print(td_list)
+webhook_url = conf['webhook']
+teams_title = "[Overtime Meal] " + taipei_date
+# TODO: mention all
+teams_subtitle = "<at>Overtime Meal</at>\n\n The Number of Users: ***{}***".format(num_users)
+teams_content = "Lists: *{}*".format(" ".join(td_list))
+send_teams(webhook_url = webhook_url, title = teams_title, subtitle = teams_subtitle, content = teams_content)
+# print(num_users)
+# print(td_list)
